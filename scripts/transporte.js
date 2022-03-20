6b3194203448e55f8598104b2e3388ff51effe50
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-03-20 15:54:12 -0400

    Agregar nombre y static a transportes

--- scripts/transporte.js
+++ scripts/transporte.js
@@ -1,10 +1,11 @@
 class Transporte {
-  transportes = {
-    moto: new Transporte(50, 50),
-    furgo: new Transporte(50, 4000)
+  static transportes = {
+    moto: new Transporte('moto', 50, 50),
+    furgo: new Transporte('furgo', 50, 4000)
   }
 
-  constructor(velocidad, capacidad) {
+  constructor(nombre, velocidad, capacidad) {
+    this.nombre = nombre
     this.velocidad = velocidad // en km/h
     this.capacidad = capacidad // en kg
   }
