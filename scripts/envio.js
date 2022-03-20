1f3bec29f69005b73ea265b2ac6ddda0f46db674
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-03-20 15:44:41 -0400

    Agregar tiempo estimado de recorrido

--- scripts/envio.js
+++ scripts/envio.js
@@ -1,18 +1,26 @@
 class Envio {
-  constructor(origen, destino, tarifa = 0.2) {
-    this.tarifa = tarifa / 100 // pasar el % a su forma decimal
+  constructor(origen, destino, transporte, tarifa) {
     this.origen = origen
     this.destino = destino
+    this.transporte = transporte
+    this.distancia = Ubicacion.calcularDistancia(this.origen, this.destino)
+    this.tarifa = tarifa / 100 // Pasar el % a su forma decimal
     this.precio = this.calcularPrecio()
+    this.tiempoEstimado = this.calcularTiempoEstimado()
   }
 
   calcularPrecio() {
     // El precio general será de 0.5$ por km de recorrido
     let precio = 0.5
     // El precio será entonces 0.5$ * la distancia en km
-    precio *= Ubicacion.calcularDistancia(this.origen, this.destino)
+    precio *= this.distancia
     // + el % de la tarifa
     precio += precio * this.tarifa
     return precio
+  }
+
+  calcularTiempoEstimado() {
+    const tiempoEstimado = this.distancia / this.transporte.velocidad
+    return tiempoEstimado
   }
 }
