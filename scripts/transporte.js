3ba7e0d7d624921d2be0cc34f86582c4e2b1e8d7
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-03-20 15:56:52 -0400

    Agregar propiedad tarifa a cada transporte

--- scripts/transporte.js
+++ scripts/transporte.js
@@ -1,12 +1,13 @@
 class Transporte {
   static transportes = {
-    moto: new Transporte('moto', 50, 50),
-    furgo: new Transporte('furgo', 50, 4000)
+    moto: new Transporte('moto', 50, 50, 0.2),
+    furgo: new Transporte('furgo', 50, 4000, 0.3)
   }
 
-  constructor(nombre, velocidad, capacidad) {
+  constructor(nombre, velocidad, capacidad, tarifa) {
     this.nombre = nombre
     this.velocidad = velocidad // en km/h
     this.capacidad = capacidad // en kg
+    this.tarifa = tarifa // en %
   }
 }
