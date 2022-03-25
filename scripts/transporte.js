9b27a2fd460457d148e55c9b65605aa24c070b7e
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-03-24 22:27:02 -0400

    Modificar valores del transporte

--- scripts/transporte.js
+++ scripts/transporte.js
@@ -1,7 +1,7 @@
 export default class Transporte {
   static transportes = {
-    moto: new Transporte('moto', 60, 50, 0.2),
-    furgo: new Transporte('furgo', 45, 4000, 0.3)
+    moto: new Transporte('moto', 50, 50, 0.1),
+    furgo: new Transporte('furgo', 45, 4000, 0.15)
   }
 
   constructor(nombre, velocidad, capacidad, tarifa) {
