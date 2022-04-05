bdf58dbd8db37d5f002f6c2f24f2ed8f3f24b9f0
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-04-04 21:20:52 -0400

    Añadir navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,24 @@
 import CalculadoraCostosEnvios from "./calculadora.js";
 import Utilidades from "./utilidades.js";
 
+// Por defecto iniciar en la vista de calculadora
 Utilidades.cambiarVista("../views/calculadora.html").then(() => {
   let calculadora = new CalculadoraCostosEnvios();
   calculadora.iniciar();
+});
+
+// Al hacer click en un link de la navbar cambiar a la vista correspondiente
+document.querySelectorAll(".navbar-nav a").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // Evita que se cambie por completo de página
+    e.preventDefault();
+
+    // Cambiar a la vista referenciada en el atributo href del link clickeado
+    Utilidades.cambiarVista(e.target.attributes.href.value).then(() => {
+      if (e.target.attributes.href.value === "./views/calculadora.html") {
+        let calculadora = new CalculadoraCostosEnvios();
+        calculadora.iniciar();
+      }
+    });
+  });
 });
