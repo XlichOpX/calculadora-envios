4c269251717aa11db8c1e3d69738f568dcc02779
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-04-05 21:58:47 -0400

    Limpieza

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,24 @@
-import CalculadoraCostosEnvios from "./calculadora.js";
-import Utilidades from "./utilidades.js";
+import CalculadoraCostosEnvios from './calculadora.js'
+import Utilidades from './utilidades.js'
 
 // Por defecto iniciar en la vista de calculadora
-Utilidades.cambiarVista("../views/calculadora.html").then(() => {
-  let calculadora = new CalculadoraCostosEnvios();
-  calculadora.iniciar();
-});
+Utilidades.cambiarVista('../views/calculadora.html').then(() => {
+  let calculadora = new CalculadoraCostosEnvios()
+  calculadora.iniciar()
+})
 
 // Al hacer click en un link de la navbar cambiar a la vista correspondiente
-document.querySelectorAll(".navbar-nav a").forEach((link) => {
-  link.addEventListener("click", (e) => {
+document.querySelectorAll('.navbar-nav a').forEach((link) => {
+  link.addEventListener('click', (e) => {
     // Evita que se cambie por completo de página
-    e.preventDefault();
+    e.preventDefault()
 
     // Cambiar a la vista referenciada en el atributo href del link clickeado
     Utilidades.cambiarVista(e.target.attributes.href.value).then(() => {
-      if (e.target.attributes.href.value === "./views/calculadora.html") {
-        let calculadora = new CalculadoraCostosEnvios();
-        calculadora.iniciar();
+      if (e.target.attributes.href.value === './views/calculadora.html') {
+        let calculadora = new CalculadoraCostosEnvios()
+        calculadora.iniciar()
       }
-    });
-  });
-});
+    })
+  })
+})
---
+++ scripts/registro.js
@@ -0,0 +1,5 @@
+export default class Registrador {
+  iniciar() {
+    this.form = document.getElementById('form-registro')
+  }
+}
