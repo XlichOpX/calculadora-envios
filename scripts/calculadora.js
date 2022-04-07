68589a6ce417aa484635df4d0429287bb5cf805b
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-04-07 11:22:42 -0400

    Borrar jQuery

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -5,25 +5,32 @@ import Envio from './envio.js'
 import Paquete from './paquete.js'
 
 export default class CalculadoraCostosEnvios {
+  constructor() {
+    this.inputs = document.getElementById('formulario').elements
+  }
   iniciar() {
     // Obtener el input peso del paquete
-    const inputPeso = $('#peso')
+    const inputPeso = this.inputs['peso']
 
     // Obtener los inputs  de dimensiones del paquete
-    const inputsDimensiones = $('input', '#dimensiones-paquete')
+    const inputsDimensiones = [
+      this.inputs['largo'],
+      this.inputs['ancho'],
+      this.inputs['alto']
+    ]
 
     // Obtener los selects del form
-    const selectOrigen = $('#select-origen')
-    const selectDestino = $('#select-destino')
-    const selectTransporte = $('#select-transporte')
+    const selectOrigen = this.inputs['select-origen']
+    const selectDestino = this.inputs['select-destino']
+    const selectTransporte = this.inputs['select-transporte']
 
     // Obtener el input de ganancia
-    const inputGanancia = $('#ganancia')
+    const inputGanancia = this.inputs['ganancia']
 
     // Obtener los recipientes para las salidas
-    const recipientePrecio = $('#precio-calculado')
-    const recipienteDistancia = $('#distancia-recorrida')
-    const recipienteTiempoEstimado = $('#tiempo-estimado')
+    const recipientePrecio = document.getElementById('precio-calculado')
+    const recipienteDistancia = document.getElementById('distancia-recorrida')
+    const recipienteTiempoEstimado = document.getElementById('tiempo-estimado')
 
     // Agregar cada ubicación de la clase Ubicacion como options a cada select
     Object.values(Ubicacion.ubicaciones).forEach((ubicacion) => {
@@ -31,20 +38,22 @@ export default class CalculadoraCostosEnvios {
       const nombreEnMayuscula = Utilidades.primeraLetraMayuscula(
         ubicacion.nombre
       )
-      selectOrigen.append(new Option(nombreEnMayuscula, ubicacion.nombre))
-      selectDestino.append(new Option(nombreEnMayuscula, ubicacion.nombre))
+      selectOrigen.appendChild(new Option(nombreEnMayuscula, ubicacion.nombre))
+      selectDestino.appendChild(new Option(nombreEnMayuscula, ubicacion.nombre))
     })
 
     Object.values(Transporte.transportes).forEach((transporte) => {
       const nombreEnMayuscula = Utilidades.primeraLetraMayuscula(
         transporte.nombre
       )
-      selectTransporte.append(new Option(nombreEnMayuscula, transporte.nombre))
+      selectTransporte.appendChild(
+        new Option(nombreEnMayuscula, transporte.nombre)
+      )
     })
 
     // Obtener el form y escuchar al evento submit
-    const form = $('#formulario')
-    form.submit((event) => {
+    const form = document.getElementById('formulario')
+    form.addEventListener('submit', (event) => {
       // Evitar que se recarge la página
       event.preventDefault()
 
@@ -55,25 +64,27 @@ export default class CalculadoraCostosEnvios {
       }
 
       // Crear un paquete
-      let paquete = new Paquete(inputPeso.val(), dimensiones)
+      let paquete = new Paquete(inputPeso.value, dimensiones)
 
       // Crear un objeto envio con los valores del form
       const envio = new Envio(
         paquete,
-        Ubicacion.ubicaciones[selectOrigen.val()],
-        Ubicacion.ubicaciones[selectDestino.val()],
-        Transporte.transportes[selectTransporte.val()],
-        inputGanancia.val()
+        Ubicacion.ubicaciones[selectOrigen.value],
+        Ubicacion.ubicaciones[selectDestino.value],
+        Transporte.transportes[selectTransporte.value],
+        inputGanancia.value
       )
 
       // Mostrar el precio calculado
-      recipientePrecio.text(`Costo del envío: $${envio.precio.toFixed(2)}`)
-      recipienteDistancia.text(
-        `Distancia a recorrer: ${envio.distancia.toFixed(2)} km`
-      )
-      recipienteTiempoEstimado.text(
-        `Tiempo de recorrido estimado: ${envio.tiempoEstimado.toFixed(2)} horas`
-      )
+      recipientePrecio.textContent = `Costo del envío: $${envio.precio.toFixed(
+        2
+      )}`
+      recipienteDistancia.textContent = `Distancia a recorrer: ${envio.distancia.toFixed(
+        2
+      )} km`
+      recipienteTiempoEstimado.textContent = `Tiempo de recorrido estimado: ${envio.tiempoEstimado.toFixed(
+        2
+      )} horas`
     })
   }
 }
