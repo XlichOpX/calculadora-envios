5ea50c3d4cf363ad8f489e7cb20b80b479035e95
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-04-18 18:56:12 -0400

    Agg funciones de codificacion base64url

---
+++ backend/utilidades/codificaciones.php
@@ -0,0 +1,10 @@
+<?php
+function base64url_encode($string)
+{
+    return rtrim(strtr(base64_encode($string), '+/', '-_'), '=');
+}
+
+function base64url_decode($string)
+{
+    return base64_decode(str_pad(strtr($string, '-_', '+/'), strlen($string) % 4, '=', STR_PAD_RIGHT));
+}
