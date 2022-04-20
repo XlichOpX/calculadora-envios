49729abde463d58913097e31989f11b8c5eb381f
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-04-20 11:17:52 -0400

    Agg direccion a los datos requeridos del usuario

--- backend/modelo/usuarios.php
+++ backend/modelo/usuarios.php
@@ -18,20 +18,38 @@ class Usuarios extends Conexion
 
     function crearUsuario($datos)
     {
-        // datos requeridos
-        $requeridos = [
+        // datos requeridos del usuario
+        $requeridos_usuario = [
             "correo_electronico" => PDO::PARAM_STR,
             "clave" => PDO::PARAM_STR,
             "nombre" => PDO::PARAM_STR,
             "apellido" => PDO::PARAM_STR,
             "cedula" => PDO::PARAM_STR,
-            "fecha_nacimiento" => PDO::PARAM_STR
+            "fecha_nacimiento" => PDO::PARAM_STR,
+            "telefono" => PDO::PARAM_STR,
+        ];
+
+        // datos requeridos para la direccion
+        $requeridos_direccion = [
+            "id_pais" => PDO::PARAM_INT,
+            "id_estado" => PDO::PARAM_INT,
+            "id_municipio" => PDO::PARAM_INT,
+            "id_parroquia" => PDO::PARAM_INT,
+            "calle" => PDO::PARAM_STR,
+            "referencia" => PDO::PARAM_STR,
         ];
 
         // verifica que existan todos los datos requeridos
-        foreach ($requeridos as $nombre => $tipo) {
+        foreach ($requeridos_usuario as $nombre => $tipo) {
             if (!isset($datos[$nombre])) {
-                return "Error: datos incompletos";
+                return "Error: datos incompletos"  . $nombre;
+            }
+        }
+
+        // verifica que existan todos los datos requeridos
+        foreach ($requeridos_direccion as $nombre => $tipo) {
+            if (!isset($datos[$nombre])) {
+                return "Error: datos incompletos: " . $nombre;
             }
         }
 
@@ -39,12 +57,33 @@ class Usuarios extends Conexion
         $datos["clave"] = password_hash($datos["clave"], PASSWORD_DEFAULT);
 
         // crear array de params para la query
-        foreach ($requeridos as $nombre => $tipo) {
+        foreach ($requeridos_usuario as $nombre => $tipo) {
             $params[] = ["nombre" => $nombre, "valor" => $datos[$nombre], "tipo" => $tipo];
         }
 
-        $query = "INSERT INTO usuarios(correo_electronico, clave ,nombre, apellido, cedula, fecha_nacimiento) VALUES (:correo_electronico, :clave, :nombre, :apellido, :cedula, :fecha_nacimiento)";
+        // query para insertar usuario
+        $query = "INSERT INTO usuarios(correo_electronico, clave ,nombre, apellido, cedula, fecha_nacimiento, telefono) VALUES (:correo_electronico, :clave, :nombre, :apellido, :cedula, :fecha_nacimiento, :telefono)";
 
-        return $this->insert($query, $params);
+        // comenzar transaccion
+        $this->conexion->beginTransaction();
+        $id_usuario = $this->insert($query, $params);
+
+        // vaciar params
+        $params = [];
+        // agg el usuario a los params
+        $params[] = ["nombre" => "id_usuario", "valor" => $id_usuario, "tipo" => PDO::PARAM_INT];
+        // crear array de params para el insert de direccion
+        foreach ($requeridos_direccion as $nombre => $tipo) {
+            $params[] = ["nombre" => $nombre, "valor" => $datos[$nombre], "tipo" => $tipo];
+        }
+
+        // query para insertar direccion
+        $query = "INSERT INTO direcciones(id_usuario, id_pais , id_estado, id_municipio, id_parroquia, calle, referencia) VALUES (:id_usuario, :id_pais , :id_estado, :id_municipio, :id_parroquia, :calle, :referencia)";
+
+        // insertar direccion
+        $this->insert($query, $params);
+
+        // finalizar transaccion y retornar resultado
+        return $this->conexion->commit();
     }
 }
