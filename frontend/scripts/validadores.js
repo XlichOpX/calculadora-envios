685d1164ff3951e2013a39626a040d815563df35
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-04-21 10:32:01 -0400

    Refactorizar validadores

--- frontend/scripts/validadores.js
+++ frontend/scripts/validadores.js
@@ -1,35 +1,31 @@
 export default class Validadores {
   // Verifica que el input no esté vacío
   static requerido(value) {
-    if (value !== "") return true;
-    return false;
+    return value !== "";
   }
 
   // Verifica que el nombre sea menor a 50 caracteres
   static nombre(nombre) {
-    if (nombre.length < 50) return true;
-    return false;
+    return nombre.length <= 50;
   }
 
   // Verifica que la cedula cumpla con el formato especificado
   static cedula(cedula) {
     const cedulaRegExp = /^[VEJvej]\d+/;
-    if (cedulaRegExp.test(cedula)) return true;
-    return false;
+    return cedulaRegExp.test(cedula);
   }
 
   // Verifica que el email cumpla con el formato especificado
   static email(email) {
     const emailRegExp =
       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if (emailRegExp.test(email)) return true;
-    return false;
+    return emailRegExp.test(email);
   }
 
   // Verifica que el telefono cumpla con el formato especificado
   static telefono(telefono) {
     const telefonoRegExp = /^[0]\d{10}/;
-    if (telefonoRegExp.test(telefono)) return true;
-    return false;
+    return telefonoRegExp.test(telefono);
+  }
   }
 }
