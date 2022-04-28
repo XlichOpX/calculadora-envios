6ca15c823e382b632b1866fc346d29ed29abcebe
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-04-28 11:11:41 -0400

    Ajustar calculo de precios

--- frontend/scripts/envios/envio.js
+++ frontend/scripts/envios/envio.js
@@ -15,10 +15,10 @@ export default class Envio {
     if (this.distancia === 0) return 2;
 
     // El precio base será de 0.02$ por km de recorrido
-    const precioBase = this.distancia * 0.02;
+    const precioBase = this.distancia * 0.01;
 
     // + el % de la tarifa
-    let precio = precioBase + precioBase;
+    let precio = precioBase;
 
     // + el % del tipo de transporte
     precio += precioBase * this.transporte.tarifa;
@@ -26,12 +26,12 @@ export default class Envio {
     // + un % del peso del paquete
     // (se toma una décima parte de gananciaConductor para
     // que el costo no sea exageradamente grande)
-    precio += this.paquete.peso * 0.01;
+    precio += this.paquete.peso * 0.000015;
 
     // + un % del volumen del paquete
     // (se toma una décima parte de gananciaConductor para
     // que el costo no sea exageradamente grande)
-    precio += this.paquete.volumen * 0.01;
+    precio += this.paquete.volumen * 0.000015;
 
     return precio;
   }
