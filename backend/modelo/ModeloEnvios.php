1572365ac119298d1e3ceb21265e87cca3209fa5
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-04-28 11:12:14 -0400

    Func crear envios

--- backend/modelo/ModeloEnvios.php
+++ backend/modelo/ModeloEnvios.php
@@ -13,14 +13,14 @@ class ModeloEnvios extends Conexion
         // campos requeridos para crear un envio
         $requeridos = [
             "usuario" => PDO::PARAM_INT,
-            "direccion" => PDO::PARAM_INT,
             "estatus" => PDO::PARAM_INT,
             "transporte" => PDO::PARAM_INT,
+            "destino" => PDO::PARAM_INT,
+            "origen" => PDO::PARAM_INT,
             "peso" => PDO::PARAM_STR,
             "ancho" => PDO::PARAM_STR,
             "alto" => PDO::PARAM_STR,
             "largo" => PDO::PARAM_STR,
-            "precio" => PDO::PARAM_STR,
             "nota_adicional" => PDO::PARAM_STR,
         ];
 
@@ -31,6 +31,58 @@ class ModeloEnvios extends Conexion
                 return ["status" => 400, "error" => "Datos incompletos"];
             }
         }
+
+        // agg el precio
+        $requeridos["precio"] = PDO::PARAM_STR;
+
+        // obt el punto de origen
+        $origen = $this->query("SELECT x, y FROM ubicaciones WHERE id = :id", [
+            [
+                "nombre" => "id",
+                "valor" => $datos["origen"],
+                "tipo" => PDO::PARAM_INT,
+            ],
+        ])[0];
+
+        // obt el punto de destino
+        $destino = $this->query("SELECT x, y FROM ubicaciones WHERE id = :id", [
+            [
+                "nombre" => "id",
+                "valor" => $datos["destino"],
+                "tipo" => PDO::PARAM_INT,
+            ],
+        ])[0];
+
+        // obt el transporte
+        $transporte = $this->query(
+            "SELECT tarifa FROM transportes WHERE id = :id",
+            [
+                [
+                    "nombre" => "id",
+                    "valor" => $datos["transporte"],
+                    "tipo" => PDO::PARAM_INT,
+                ],
+            ],
+        )[0];
+
+        // calcular la distancia entre ambos puntos
+        $distancia = sqrt(
+            ($destino["x"] - $origen["x"]) ** 2 +
+                ($destino["y"] - $origen["y"]) ** 2,
+        );
+
+        // precio base de 0.01$ por km
+        $precio_base = $distancia * 0.01;
+        // agg la tarifa del transporte
+        $precio = $precio_base + $precio_base * $transporte["tarifa"];
+        // agg una parte del peso al precio
+        $precio += $datos["peso"] * 0.000015;
+        // agg una parte del volumen al precio
+        $precio +=
+            $datos["alto"] * $datos["ancho"] * $datos["largo"] * 0.000015;
+
+        // agg el precio calculado a los datos
+        $datos["precio"] = $precio;
 
         // crear array de params
         foreach ($datos as $campo => $valor) {
@@ -45,9 +97,10 @@ class ModeloEnvios extends Conexion
         $query = <<<SQL
         INSERT INTO envios (
                 id_usuario,
-                id_direccion,
                 id_estatus,
                 id_transporte,
+                id_origen,
+                id_destino,
                 peso,
                 ancho,
                 alto,
@@ -58,9 +111,10 @@ class ModeloEnvios extends Conexion
 
             VALUES (
                 :usuario,
-                :direccion,
                 :estatus,
                 :transporte,
+                :origen,
+                :destino,
                 :peso,
                 :ancho,
                 :alto,
